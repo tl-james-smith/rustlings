ee1682f14c2ff130aaab5a8f48ca8db90f346299
James Smith <james.smith@doctorlink.com>
2020-02-08 21:29:50 +0000

    Complete primitive types examples

--- exercises/primitive_types/primitive_types5.rs
+++ exercises/primitive_types/primitive_types5.rs
@@ -1,12 +1,10 @@
 // primitive_types5.rs
 // Destructure the `cat` tuple so that the println will work.
-// Execute `rustlings hint primitive_types5` for hints!
-
-// I AM NOT DONE
+// Execute `rustlings hint primitive_types5` for hints!x
 
 fn main() {
     let cat = ("Furry McFurson", 3.5);
-    let /* your pattern here */ = cat;
+    let (name, age) = cat;
 
     println!("{} is {} years old.", name, age);
 }
